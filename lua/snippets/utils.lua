8803c1b69685549165ef8455d3eba4c5e26440f1
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2020-08-19 03:26:23 +0900

    Add match_comment and more utils.

--- lua/snippets/utils.lua
+++ lua/snippets/utils.lua
@@ -1,13 +1,38 @@
 local U = require 'snippets.common'
 local parser = require 'snippets.parser'
+local vim = vim
+local api = vim.api
+local nvim_get_current_line = api.nvim_get_current_line
 local format = string.format
 local concat = table.concat
 local insert = table.insert
+local min = math.min
 
 local function identity1(x) return x.v end
 
-local function line_indent()
-  return vim.api.nvim_get_current_line():match("^%s+") or ""
+local function get_line_indent()
+  return nvim_get_current_line():match("^%s+") or ""
+end
+
+local function get_line_comment()
+  local cms = vim.bo.commentstring
+  -- This whole pesc dance is a bummer.
+  local pattern = "^(%s*)"..
+    (U.find_sub(vim.pesc((U.find_sub(cms, "\0", "%s", 1, true))), "(%s*).*", "\0", 1, true))
+  local pre_ws, inner_ws = nvim_get_current_line():match(pattern)
+  if pre_ws then
+    return pre_ws..cms:format("")..inner_ws
+  end
+  return ""
+end
+
+local function once(fn, N)
+  assert(type(fn) == 'function')
+  local value
+  return function(...)
+    if not value then value = {fn(...)} end
+    return unpack(value, 1, N or #value)
+  end
 end
 
 local function into_snippet(s)
@@ -17,13 +42,29 @@ local function into_snippet(s)
   return U.make_snippet(s)
 end
 
-local function match_indentation(s)
-  local S = into_snippet(s)
-  
-  local indent = U.make_preorder_function_component(line_indent)
-  -- Large negative number so it's evaluated first ish.
-  indent.id = -123456789
+local function lowest_id(s)
+  assert(U.is_snippet(s))
+  local id
+  for i, v in ipairs(s) do
+    if U.is_variable(v) then
+      if id then
+        id = min(v.id, id)
+      else
+        id = v.id
+      end
+    end
+  end
+  if id and id >= 0 then
+    return -1
+  end
+  return id or -1
+end
 
+local function prefix_lines_with_function(s, fn)
+  local S = into_snippet(s)
+  local prefix_var = U.make_preorder_function_component(fn)
+  -- Use a unique negative number so it's evaluated first.
+  prefix_var.id = lowest_id(S) - 1
   local R = {}
   for _, v in ipairs(S) do
     if type(v) == 'string' then
@@ -31,19 +72,19 @@ local function match_indentation(s)
       insert(R, lines[1])
       for i = 2, #lines do
         insert(R, '\n')
-        insert(R, indent)
+        insert(R, prefix_var)
         insert(R, lines[i])
       end
     else
       local existing_transform = v.transform or identity1
-      -- Add indentation to any variables which have newlines.
+      -- Add prefix to any variables which have NLs.
       v.transform = function(S)
-        -- Lookup the existing indentation created by our variable.
-        local indentation = S[indent.id]
+        -- Lookup the existing prefix created by our variable.
+        local prefix = S[prefix_var.id]
         local value = existing_transform(S)
         local lines = vim.split(value, '\n', true)
         for i = 2, #lines do
-          lines[i] = indentation..lines[i]
+          lines[i] = prefix..lines[i]
         end
         return concat(lines, '\n')
       end
@@ -53,7 +94,29 @@ local function match_indentation(s)
   return U.make_snippet(R)
 end
 
+local function match_indentation(s)
+  return prefix_lines_with_function(s, get_line_indent)
+end
+
+local function match_comment(s)
+  return prefix_lines_with_function(s, get_line_comment)
+end
+
+local function match_comment_or_indentation(s)
+  return prefix_lines_with_function(s, function()
+    local comment = get_line_comment()
+    if comment == "" then
+      return get_line_indent()
+    end
+    return comment
+  end)
+end
+
 return {
   match_indentation = match_indentation;
+  match_comment = match_comment;
+  match_comment_or_indentation = match_comment_or_indentation;
   into_snippet = into_snippet;
+  lowest_id = lowest_id;
+  prefix_lines_with_function = prefix_lines_with_function;
 }
