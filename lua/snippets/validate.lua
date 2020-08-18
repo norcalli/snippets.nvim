438272ca59da1c80607300fb87ea633e9b7ade08
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2020-08-19 04:05:34 +0900

    Pee on all the files. (Add GPL header)

--- lua/snippets/validate.lua
+++ lua/snippets/validate.lua
@@ -1,3 +1,20 @@
+-- Copyright (C) 2020 Ashkan Kiani
+-- This is part of the snippets.nvim distribution.
+-- https://github.com/norcalli/snippets.nvim
+-- 
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 -- TODO(ashkan, Tue 18 Aug 2020 09:39:52 AM JST) delete this and use neovim-plugin instead.
 local type_names = {
   t='table', s='string', n='number', b='boolean', f='function', c='callable',
