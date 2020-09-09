7b5fd8071d4fb6fa981a899aae56b55897c079fd
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2020-09-10 08:42:27 +0900

    Add deepcopy.

---
+++ lua/snippets/deepcopy.lua
@@ -0,0 +1,23 @@
+-- http://lua-users.org/wiki/CopyTable
+-- Save copied tables in `copies`, indexed by original table.
+local function deepcopy(orig, copies)
+    copies = copies or {}
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        if copies[orig] then
+            copy = copies[orig]
+        else
+            copy = {}
+            copies[orig] = copy
+            for orig_key, orig_value in next, orig, nil do
+                copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
+            end
+            setmetatable(copy, deepcopy(getmetatable(orig), copies))
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+return deepcopy
